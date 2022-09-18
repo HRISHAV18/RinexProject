9d91eb64bf2b185533db42ebc300a3ffad9bc995
HRISHAV18 <97503756+HRISHAV18@users.noreply.github.com>
2022-09-18 20:38:14 +0530

    Create app.py

---
+++ MAJOR PROJECT 1/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import joblib
+
+#load the joblib model
+model_nb  = joblib.load('gas-diesel')
+
+st.title('GAS-DIESEL CLASSIFIER')
+ip = st.text_input('Enter your text :')
+
+op = model_nb.predict([ip])
+if st.button('PREDICT'):
+  st.title(op[0])
